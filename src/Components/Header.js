810df792e66ea8f4636bc8f6613a20d9d5dc9777
killeraliens <killeraliens@outlook.com>
2020-05-17 08:11:46 -0600

    organized

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import socketIOClient from "socket.io-client";
-import config from "./config"
+import config from "../config"
 
 let socket
 function Header() {
