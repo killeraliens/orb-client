3b01add0312a1aae46b67a5c0070b81f74203ab0
killeraliens <killeraliens@outlook.com>
2020-05-17 19:22:06 -0600

    before appcontext

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import socketIOClient from "socket.io-client";
 import config from "../config"
 
 let socket
 function Header() {
-  const [query, setQuery] = useState('?symbol=abc')
-  const [endpoint, setEndpoint] = useState(`${ config.SERVER_ENDPOINT + query }`)
+  // const [query, setQuery] = useState('?symbol=abc')
+  const [endpoint, setEndpoint] = useState(`${ config.SERVER_ENDPOINT }`)
   socket = socketIOClient(endpoint)
   return(
     <header>
       <h1>Killeraliens Stock Tweets</h1>
       <NavLink to="/">Date</NavLink>
       {' '}
-      <NavLink to="tweets" >Tweets</ NavLink>
+      <NavLink to="/tweets" >Tweets</ NavLink>
     </header>
   )
 }
--- src/Components/TweetFeed.js
+++ src/Components/TweetFeed.js
@@ -3,21 +3,35 @@ import React, { useState, useEffect } from 'react'
 import { socket } from './Header'
 
 export default function TweetFeed() {
-  const [data, setData] = useState('')
+  const [data, setData] = useState([])
 
   useEffect(() => {
-    socket.on('get_tweets', data => {
-      console.log('tweet api hits', data)
-      setData(data)
+    socket.on('allTweets', tweetArr => {
+      console.log('tweet api hits', tweetArr)
+      setData(tweetArr)
     })
+
     return () => {
-      socket.off('get_tweets')
+      socket.off('allTweets')
+    }
+  }, [])
+
+  useEffect(() => {
+      socket.on('tweet', tweet => {
+        console.log('new tweet', tweet)
+        console.log('existing data', data)
+        const newTweetArr = [ tweet, ...data ]
+        console.log('NEW ARR', newTweetArr)
+        setData(newTweetArr)
+      })
+    return () => {
+      socket.off('tweet')
     }
   }, [])
 
   return(
     <div className="TweetFeed">
-      {data}
+      {data.map((tweet, i) => <div key={i}>{tweet.text}</div>)}
     </div>
   )
 }
