ed403299b0bbfdf3f7a855c56f20638f28d1429e
killeraliens <killeraliens@outlook.com>
2020-05-17 23:15:26 -0600

    react_app env var name

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   SERVER_ENDPOINT: process.env.NODE_ENV === 'production'
-    ? process.env.SERVER_ENDPOINT
+    ? process.env.REACT_APP_SERVER_ENDPOINT
     : 'http://localhost:8000'
 }
 
