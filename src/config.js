a287eb14e033dc56a3a8b8043806b1ac2f9487e7
killeraliens <killeraliens@outlook.com>
2020-05-17 22:19:13 -0600

    correct env var name

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   SERVER_ENDPOINT: process.env.NODE_ENV === 'production'
-    ? process.env.local.SERVER_ENDPOINT
+    ? process.env.SERVER_ENDPOINT
     : 'http://localhost:8000'
 }
 
